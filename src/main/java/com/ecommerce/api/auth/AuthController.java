4a842ab92d826aef21fbd10962fc3e0581164ff1
audyafiary11 <audy@infini88.net>
2025-09-14 14:51:42 +0400

    init controller

--- src/main/java/com/ecommerce/api/auth/AuthController.java
+++ src/main/java/com/ecommerce/api/auth/AuthController.java
@@ -1,4 +1,26 @@
 package com.ecommerce.api.auth;
 
+import com.ecommerce.api.dto.request.RegisterRequest;
+import com.ecommerce.api.dto.response.BaseResponse;
+import com.ecommerce.api.dto.response.RegisterResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<BaseResponse<RegisterResponse>> register(@RequestBody RegisterRequest request) {
+        BaseResponse<RegisterResponse> response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
 }
