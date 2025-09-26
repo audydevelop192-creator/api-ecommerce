6a225c8481995861ba63d2f48794ca248fdeb1c1
azhararief <azhararief13@gmail.com>
2025-09-26 19:33:49 +0700

    addAddress

---
+++ src/main/java/com/ecommerce/api/address/AddressController.java
@@ -0,0 +1,35 @@
+package com.ecommerce.api.address;
+
+import com.ecommerce.api.auth.AuthService;
+import com.ecommerce.api.dto.request.AddAddressRequest;
+import com.ecommerce.api.dto.request.RegisterRequest;
+import com.ecommerce.api.dto.response.AddAddressResponse;
+import com.ecommerce.api.dto.response.BaseResponse;
+import com.ecommerce.api.dto.response.RegisterResponse;
+import com.ecommerce.api.model.Address;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/addresses")
+public class AddressController {
+
+    private final AddressService addressService;
+
+    public AddressController(AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<BaseResponse<AddAddressResponse>> addAddress(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestBody AddAddressRequest request) {
+
+        // biasanya format header = "Bearer <token>"
+        String token = authHeader.replace("Bearer ", "");
+
+        BaseResponse<AddAddressResponse> response = addressService.addAddress(token, request);
+        return ResponseEntity.ok(response);
+    }
+}
