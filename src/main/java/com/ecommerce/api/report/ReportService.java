593be47bc9dee526daa5f18a2fb6999dd8c7954b
azhararief <azhararief13@gmail.com>
2025-10-15 17:59:28 +0700

    fix voucher Usage Report

--- src/main/java/com/ecommerce/api/report/ReportService.java
+++ src/main/java/com/ecommerce/api/report/ReportService.java
@@ -3,9 +3,11 @@ package com.ecommerce.api.report;
 import com.ecommerce.api.config.AuthenticatedUser;
 import com.ecommerce.api.dto.request.RevenueByPeriodReportRequest;
 import com.ecommerce.api.dto.request.StockReportRequest;
+import com.ecommerce.api.dto.request.VoucherUsageReportRequest;
 import com.ecommerce.api.dto.response.BaseResponse;
 import com.ecommerce.api.dto.response.RevenueByPeriodReportResponse;
 import com.ecommerce.api.dto.response.StockReportResponse;
+import com.ecommerce.api.dto.response.VoucherUsageReportResponse;
 import com.ecommerce.api.utils.SecurityUtils;
 import org.springframework.stereotype.Service;
 
@@ -48,4 +50,18 @@ public class ReportService {
         List<RevenueByPeriodReportResponse> revenueReport = reportRepository.findRevenueReport(period);
         return new BaseResponse<>("success", "Revenue report retrieved successfully", revenueReport);
     }
+    public BaseResponse<List<VoucherUsageReportResponse>>voucherUsageReport(VoucherUsageReportRequest request, String startDate,String endDate){
+        AuthenticatedUser authenticatedUser = SecurityUtils.getCurrentUser();
+        if (authenticatedUser == null){
+            return new BaseResponse<>("error", "Invalid or expired token", null);
+        }
+        if (!authenticatedUser.getRole().equalsIgnoreCase("ADMIN")){
+            return new BaseResponse<>("error", "Invalid user access", null);
+        }
+        List<VoucherUsageReportResponse> voucherUsageReport = reportRepository.findVoucherUsageReport(startDate, endDate);
+        return new BaseResponse<>("success","Voucher usage report retrieved successfully",voucherUsageReport);
+
+
+    }
+
 }
